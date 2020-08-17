d7c14d02a7fe6e6f69c31d5bd6093f83dd4a24f5
Jon Phoenix <jonathan.martb@gmail.com>
2020-08-16 21:21:15 -0700

    Updating user questions as a function

--- index.js
+++ index.js
@@ -3,69 +3,70 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 const genMarkdown = require('./utils/generateMarkdown');
 
-// Array of Questions for User
-// const questions =
+// Function for User Questions and Array of Objects
+function userQuestions() {
 
-inquirer.prompt([
-    {
-        type : 'input',
-        message : 'Project Title?',
-        name : 'title',
-    },
-    {
-        type : 'input',
-        message : 'Project description?',
-        name : 'description',
-    },
-    {
-        type : 'input',
-        message : "Installation requirements?",
-        name : 'installation',
-    },
-    {
-        type : 'input',
-        message : 'How this application can be used?',
-        name : 'usage',
-    },
-    {
-        type : 'list',
-        message : 'What license fits this project?',
-        name : 'license',
-        choices : [
-            'Apache 2.0',
-            'MIT',
-            'GPL v3',
-            'ISC',
-            'BSD 3',
-            'None'
-        ],
-    },
-    {
-        type : 'input',
-        message : 'Who are the contributors?',
-        name : 'contributing',
-    },
-    {
-        type : 'input',
-        message : 'Command to run a test?',
-        name : 'tests',
-    },
-    {
-        type : 'input',
-        message : 'Your GitHub username?',
-        name : 'username',
-    },
-    {
-        type : 'input',
-        message : 'Project URL in GitHub?',
-        name : 'deployment',
-    },
-    {
-        type : 'input',
-        message : 'Your email address?',
-        name : 'email',
-    }
-]);
+    return inquirer.prompt([
+        {
+            type : 'input',
+            message : 'Project Title?',
+            name : 'title',
+        },
+        {
+            type : 'input',
+            message : 'Project description?',
+            name : 'description',
+        },
+        {
+            type : 'input',
+            message : "Installation requirements?",
+            name : 'installation',
+        },
+        {
+            type : 'input',
+            message : 'How this application can be used?',
+            name : 'usage',
+        },
+        {
+            type : 'list',
+            message : 'What license fits this project?',
+            name : 'license',
+            choices : [
+                'Apache 2.0',
+                'MIT',
+                'GPL v3',
+                'ISC',
+                'BSD 3',
+                'None'
+            ],
+        },
+        {
+            type : 'input',
+            message : 'Who are the contributors?',
+            name : 'contributing',
+        },
+        {
+            type : 'input',
+            message : 'Command to run a test?',
+            name : 'tests',
+        },
+        {
+            type : 'input',
+            message : 'Your GitHub username?',
+            name : 'username',
+        },
+        {
+            type : 'input',
+            message : 'Project URL in GitHub?',
+            name : 'deployment',
+        },
+        {
+            type : 'input',
+            message : 'Your email address?',
+            name : 'email',
+        }
+    ]);
+};
 
 // Function to write README file
 function writeToFile(fileName, data) {
