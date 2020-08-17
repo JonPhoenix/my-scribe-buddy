9e64cb192af1eacebab7ac5aad3eec45cca35887
Jon Phoenix <jonathan.martb@gmail.com>
2020-08-16 19:22:05 -0700

    Updating questions for user

--- index.js
+++ index.js
@@ -1,11 +1,49 @@
 // ES6+ Const declaration block
-const fs = require("fs");
-const inquirer = require("inquirer");
-const genMarkdown = require("./utils/generateMarkdown");
+const fs = require('fs');
+const inquirer = require('inquirer');
+const genMarkdown = require('./utils/generateMarkdown');
 
 // Array of Questions for User
 const questions = [
-
+    {
+        type : 'input',
+        message : 'Project Title?',
+        name : 'title',
+    },
+    {
+        type : 'input',
+        message : 'Project description?',
+        name : 'description',
+    },
+    {
+        type : 'input',
+        message : "Installation requirements?",
+        name : 'installation',
+    },
+    {
+        type : 'input',
+        message : 'How this project can be used?',
+        name : 'usage',
+    },
+    {
+        type : 'list',
+        message : 'What license fits this project?',
+        name : 'license',
+        choices : [
+            'MIT',
+            'None'
+        ],
+    },
+    {
+        type : 'input',
+        message : 'Who are the contributors?',
+        name : 'contributing',
+    },
+    {
+        type : 'input',
+        message : 'Command to run a test?',
+        name : 'test',
+    },
 ];
 
 // Function to write README file
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,4 +1,4 @@
-// function to generate markdown for README
+// Function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
